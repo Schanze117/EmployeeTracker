eb061726c504fd86c379e280cea154fa5ebc62e5
Schanze117 <aaronschanzenbach@gmail.com>
2025-02-16 21:01:04 -0700

    cleaned up the seed.sql file

--- db/seed.sql
+++ db/seed.sql
@@ -5,16 +5,28 @@ VALUES ('Engineering'),
        ('Legal');
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ('Software Engineer', 100000, 1)
+VALUES ('Project Manager', 140000, 1),
        ('Lead Engineer', 120000, 1),
+       ('Software Engineer', 100000, 1)
        ('Accountant', 80000, 2),
-       ('Lawyer', 95000, 2),
        ('Salesperson', 60000, 3),
        ('Sales Lead', 75000, 3);
+       ('Lawyer', 95000, 4),
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ('Alice', 'Johnson', 1, NULL),
-       ('Bob', 'Smith', 1, 1),
-       ('Charlie', 'Brown', 1, 1),
-       ('David', 'Jones', 1, 3),
-       ('Eve', 'Williams', 1, 3);
+       ('Bob', 'Smith', 2, 1),
+       ('Charlie', 'Brown', 2, 1),
+       ('Nina', 'Parker', 6, 1),
+       ('Judy', 'Lopez', 5, 2),
+       ('Kevin', 'Harris', 5, 2),
+       ('David', 'Jones', 3, 2),
+       ('Eve', 'Williams', 3, 2);
+       ('Frank', 'Miller', 3, 2),
+       ('Grace', 'Davis', 3, 3),
+       ('Heidi', 'Young', 3, 3),
+       ('Ivan', 'King', 3, 3),
+       ('Larry', 'Nelson', 4, 1),
+       ('Molly', 'Olson', 4, 1),
+       ('Nancy', 'Perez', 6, NULL),
+      
